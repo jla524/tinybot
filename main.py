282070888011af31514d767f76680e6af5627b86
Jacky Lee <jla524@sfu.ca>
2023-05-31 17:40:28 -0700

    Can post comments in GitHub PR

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from dotenv import dotenv_values
 
 Json: TypeAlias = dict[str, Any]
+Lines: TypeAlias = list[tuple[str, int, int]]  # filename, lines added, lines deleted
 config: Json = dotenv_values(".env")
 
 class TinyBot:
@@ -13,8 +14,8 @@ class TinyBot:
     "X-GitHub-Api-Version": "2022-11-28",
   }
 
-  def __init__(self, owner: str = "geohot", repo: str = "tinygrad"):
-    self.owner, self.repo = owner, repo
+  def __init__(self, owner: str = "geohot", repo: str = "tinygrad", project_dir: str = "tinygrad/"):
+    self.owner, self.repo, self.project_dir = owner, repo, project_dir
     self.base_url = f"https://api.github.com/repos/{owner}/{repo}"
 
   def list_prs(self) -> Json:
@@ -30,20 +31,40 @@ class TinyBot:
       files[url] = response.json()
     return files
   
-  def get_lines(self, pr_files: list[Json], file_start: str = "tinygrad/") -> dict[str, tuple[str, int, int]]:
+  def get_lines(self, pr_files: list[Json]) -> dict[str, Lines]:
     lines = defaultdict(list)
     for url, files in pr_files.items():
+      total_additions, total_deletions = 0, 0
       for file in files:
-        if file["filename"].startswith(file_start):
+        if file["filename"].startswith(self.project_dir):
+          total_additions += file["additions"]
+          total_deletions += file["deletions"]
           lines[url].append((file["filename"], file["additions"], file["deletions"]))
     return lines
 
+  def comment(self, pr_lines: dict[str, Lines]):
+    """
+    $ curl -s -H "Authorization: token ${ACCESS_TOKEN}" \
+    -X POST -d '{"body": "Your Message to Comment"}' \
+    "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments"
+    """
+    for url, lines in pr_lines.items():
+      post_url = url.replace("pulls", "issues") + "/comments"
+      comment = f"Changes made in `{self.project_dir}`:\n"
+      comment += "```\n"
+      comment += "-" * 57 + "\n"
+      comment += "files" + " " * 27 + "additions       deletions\n"
+      comment += "-" * 57 + "\n"
+      for fn, additions, deletions in lines:
+        if fn == "total": comment += "-" * 57 + "\n"
+        comment += f"{fn:<35} {additions:>5} {deletions:>15}\n"
+      comment += "```\n"
+      response = requests.post(post_url, json={"body": comment}, headers=self.headers, allow_redirects=True)
+      assert response.status_code == 201
+
 if __name__ == "__main__":
   tinybot = TinyBot()
-  pr_urls = [x["url"] for x in tinybot.list_prs()]
+  pr_urls = [x["url"] for x in tinybot.list_prs() if x["user"]["login"] == "jla524"]  # testing
   pr_files = tinybot.list_pr_files(pr_urls)
-  for url, files in tinybot.get_lines(pr_files).items():
-    print(url)
-    for fn, added, deleted in files:
-      print(f"{fn}\tadded {added}\tdeleted {deleted}")
-    print()
+  pr_lines = tinybot.get_lines(pr_files)
+  tinybot.comment(pr_lines)
